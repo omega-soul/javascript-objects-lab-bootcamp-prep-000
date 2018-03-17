11981eef4a4eb45b4796c44b9fe7a5d64b762478
vincent <omegaortiz1997@gmail.com>
2018-03-17 18:21:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
-
+function deleteFromObjectByKey(object, key) {
+  delete object.key; 
+  return {};
+}
 
 function destructivelyDeleteFromObjectByKey(object, key) {
  delete object[key];
